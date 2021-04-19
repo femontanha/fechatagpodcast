4ba98463c8ec9e9de03f983fdeb90635b5fb59ac
Fellipe Azambuja <femontanhanew@gmail.com>
2021-04-19 00:42:11 -0300

    New episode 05

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,9 @@ export default function Home() {
       <div className={styles.host}>
         <h2 className={styles.hostTitle}>último episódio</h2>
         <div className={styles.player}>
-          <img src="https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_episode/13565104/13565104-1618199600138-3f6653a57ef52.jpg" alt="Keit Oliveira" />
+          <img src="https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_episode/13565104/13565104-1618803486403-00bbbcd41f959.jpg" alt="Pokemaobr" />
           <AudioPlayer
-            src="https://anchor.fm/s/51734b40/podcast/play/31232913/https%3A%2F%2Fd3ctxlq1ktw2nl.cloudfront.net%2Fstaging%2F2021-3-12%2F1cc4d1e3-3e3d-9645-a2cd-31c655b81a1c.mp3"
+            src="https://anchor.fm/s/51734b40/podcast/play/31712934/https%3A%2F%2Fd3ctxlq1ktw2nl.cloudfront.net%2Fstaging%2F2021-3-19%2Fb902b36f-b5d1-d72e-9c55-06f43a049310.mp3"
             showFilledVolume
             customAdditionalControls={[]}
           />
