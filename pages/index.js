9e40eaa116cad7721fd1a69a4428d99a491d07ff
Fellipe Azambuja <femontanhanew@gmail.com>
2021-05-10 00:39:23 -0300

    New episode 08

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,16 @@ export default function Home() {
         </p>
       </div>
       <div className={styles.host}>
-        <h2 className={styles.hostTitle}>último episódio</h2>
+        <h2 className={styles.hostTitle}>último episódio em vídeo</h2>
+        <div className={styles.hostVideo}>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/TmXf4aR1wx0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+
+        <h2 className={styles.hostTitle}>último episódio em áudio</h2>
         <div className={styles.player}>
-          <img src="https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_episode/13565104/13565104-1620019142107-e7021963dd9a9.jpg" alt="Felipe Fialho" />
+          <img src="https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_episode/13565104/13565104-1620617059102-b88ec29a0282a.jpg" alt="Gabriel Froes" />
           <AudioPlayer
-            src="https://anchor.fm/s/51734b40/podcast/play/32711790/https%3A%2F%2Fd3ctxlq1ktw2nl.cloudfront.net%2Fstaging%2F2021-4-3%2F8089dfd5-d755-9c46-7533-30b24a505b63.mp3"
+            src="https://anchor.fm/s/51734b40/podcast/play/33184550/https%3A%2F%2Fd3ctxlq1ktw2nl.cloudfront.net%2Fstaging%2F2021-4-10%2F93d1bc6f-6202-eb04-26c2-c54353b3d4b1.mp3"
             showFilledVolume
             customAdditionalControls={[]}
           />
