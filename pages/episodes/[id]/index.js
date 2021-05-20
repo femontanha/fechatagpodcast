8105d86e55d15803fb33edcbee8bf458534921b8
Fellipe Azambuja <femontanhanew@gmail.com>
2021-05-20 08:18:48 -0300

    Add player

--- pages/episodes/[id]/index.js
+++ pages/episodes/[id]/index.js
@@ -1,18 +1,38 @@
 import Parser from 'rss-parser'
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css'
+
+import Block from '../../../components/block'
+import Social from '../../../components/social'
 
 import styles from './pageEpisode.module.css';
 
 const parser = new Parser()
 
-const PageEpisode = ({ title, content, pubDate }) => {
+const PageEpisode = ({ title, content, pubDate, enclosure }) => {
+  console.log(enclosure.url);
   return (
-    <section className="container">
-      <time className={styles.time}>{new Date(pubDate).toLocaleDateString('pt-br')}</time>
-      <h2 className={styles.title}>{title}</h2>
-      <div dangerouslySetInnerHTML={{
-        __html: content
-      }}></div>
-    </section>
+    <>
+      <section className="container">
+        <time className={styles.time}>{new Date(pubDate).toLocaleDateString('pt-br')}</time>
+        <h2 className={styles.title}>{title}</h2>
+        <AudioPlayer
+          src={enclosure.url}
+          showFilledVolume
+          customAdditionalControls={[]}
+        />
+        <div dangerouslySetInnerHTML={{
+          __html: content
+        }}></div>
+      </section>
+      <Block
+        title="redes sociais"
+        description="acesse as nossas redes sociais"
+        inverted
+      >
+        <Social />
+      </Block>
+    </>
   );
 }
 
